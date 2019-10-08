6849fbc9e4e51bc9600435626048060eba40b87b
gruntultra <hakwee123@yahoo.com.sg>
2019-10-08 13:50:32 +0800

    Updates

---
+++ precache-manifest.4c4f30e882b9e2113d4051ff86f25425.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a04c94dfbabff18274660d75abf92a3",
+    "url": "/intranet-proto/index.html"
+  },
+  {
+    "revision": "b1f190b855814d4dbefd",
+    "url": "/intranet-proto/static/css/main.2776444d.chunk.css"
+  },
+  {
+    "revision": "3cca6135f17fdf9238b1",
+    "url": "/intranet-proto/static/js/2.dc1c41cc.chunk.js"
+  },
+  {
+    "revision": "b1f190b855814d4dbefd",
+    "url": "/intranet-proto/static/js/main.e306040e.chunk.js"
+  },
+  {
+    "revision": "e8464d4abb4ffcc9eb18",
+    "url": "/intranet-proto/static/js/runtime-main.f38cfd63.js"
+  },
+  {
+    "revision": "13f4793be54fb9f98a47c4668bd844bb",
+    "url": "/intranet-proto/static/media/rag.13f4793b.png"
+  },
+  {
+    "revision": "31e4fe513f454fb437d1810a2de2f9f1",
+    "url": "/intranet-proto/static/media/sheares-hall-logo.31e4fe51.png"
+  },
+  {
+    "revision": "3e97d523d1620ee2a3f1f035e395744d",
+    "url": "/intranet-proto/static/media/uname.3e97d523.png"
+  }
+]);
